ef15f28257ddd878d12caa5bcc046664b83e1888
Kyle Zhang <kyleozhang@gmail.com>
2014-03-12 22:40:22 -0400

    Formatting

--- arena.lua
+++ arena.lua
@@ -16,99 +16,96 @@ PLUGIN.theKits[3] = { "Millitary" , { 199, "9mm Ammo"}, { 1, "MP54A"}, { 5, "Lar
 
 -- Initializes the plugin
 function PLUGIN:Init()
-   Arena = self
-   Arena.isOn = false
-   Arena:AddChatCommand( "startarena", Arena.arenaON )
-   Arena:AddChatCommand( "stoparena", Arena.arenaOFF )
-   Arena:AddChatCommand( "join", Arena.arenaPort )
-   Arena:AddChatCommand( "coord", Arena.Coord )
-   Arena:AddChatCommand( "arena", Arena.displayArena )
-   Arena:AddChatCommand( "ahelp", Arena.AHELP )
+    Arena = self
+    Arena.isOn = false
+    Arena:AddChatCommand( "startarena", Arena.arenaON )
+    Arena:AddChatCommand( "stoparena", Arena.arenaOFF )
+    Arena:AddChatCommand( "join", Arena.arenaPort )
+    Arena:AddChatCommand( "coord", Arena.Coord )
+    Arena:AddChatCommand( "arena", Arena.displayArena )
+    Arena:AddChatCommand( "ahelp", Arena.AHELP )
 end
 
 function PLUGIN:arenaON( netuser )
-   if ( not(netuser:CanAdmin()) ) then
-  rust.Notice( netuser, "Only admins can do this!" )
-  return
-  end
-   Arena.isOn= true
-   Arena.playerList = {}
-   message = "Arena open! Type /join to join ( BEWARE -- your inventory will be cleared )"
-   rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
-   timer.Once(15, function()
-      message = "Arena will start in 2 mins !"
-      rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
-      timer.Once(15, function()
-      	 message = "Arena will start in 1 min !"
-         rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
-         timer.Once(15, function()
-            -- start the game
-            message = "Handing out your weapons..."
-            rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
-            Arena.playing = true
-            Arena:givePlayerKits()
-            timer.Once(15, function()
-               message = "FIGHT !!!"
-               rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
-            end)
-         end)
-      end)
-   end)
-   return
+    if ( not(netuser:CanAdmin()) ) then
+        rust.Notice( netuser, "Only admins can do this!" )
+    else
+        Arena.isOn= true
+        Arena.playerList = {}
+        message = "Arena open! Type /join to join ( BEWARE -- your inventory will be cleared )"
+        rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
+        --First notification
+        timer.Once(15, function()
+        message = "Arena will start in 2 mins !"
+        rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
+        --Second notification
+        timer.Once(15, function()
+      	message = "Arena will start in 1 min !"
+        rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
+        --Distribute kits
+        timer.Once(15, function()       
+        message = "Handing out your weapons..."
+        rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
+        Arena.playing = true
+        Arena:givePlayerKits()
+        --Start the arena
+        timer.Once(15, function()
+        message = "FIGHT !!!"
+        rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
+        end) end) end) end)
+    end
+    return
 end
 
 function PLUGIN:arenaOFF( netuser )
-   if ( not(netuser:CanAdmin()) ) then
-  rust.Notice( netuser, "Only admins can do this!" )
-  return
-  end
-   Arena.isOn= false
-   Arena.playing = false
-   message = "Arena is closed!"
-   rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
-   return
+    if ( not(netuser:CanAdmin()) ) then
+        rust.Notice( netuser, "Only admins can do this!" )
+    else
+        Arena.isOn= false
+        Arena.playing = false
+        message = "Arena is closed!"
+        rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
+    end 
+    return
 end
 
 function PLUGIN:arenaPort( netuser )
-   if (Arena.isOn== true and Arena.playing== false) then
-     Arena:clearInventory(netuser)
-     local coords = netuser.playerClient.lastKnownPosition
-     coords.x = 3440.619628903 --Xcoordinates arena
-     coords.y = 353.20803833008 --Ycoordinates arena
-     coords.z = 1167.337890625 --Zcoordinates arena
-     rust.ServerManagement():TeleportPlayer(netuser.playerClient.netPlayer, coords)
-	 table.insert( Arena.playerList, netuser )
-	 table.insert( Arena.alivePlayers, netuser )
-   else
-     rust.Notice(netuser, "Arena is closed!")
-   end
-   return
+    if (Arena.isOn== true and Arena.playing== false) then
+        Arena:clearInventory(netuser)
+        local coords = netuser.playerClient.lastKnownPosition
+        coords.x = 3440.619628903 --Xcoordinates arena
+        coords.y = 353.20803833008 --Ycoordinates arena
+        coords.z = 1167.337890625 --Zcoordinates arena
+        rust.ServerManagement():TeleportPlayer(netuser.playerClient.netPlayer, coords)
+	      table.insert( Arena.playerList, netuser )
+	      table.insert( Arena.alivePlayers, netuser )
+    else rust.Notice(netuser, "Arena is closed!") end
+    return
 end
 
 function PLUGIN:clearInventory( netuser )
-  local inv = rust.GetInventory(netuser)
-  if (not inv) then return end
-  for i=0,39 do
-    local b, item = inv:GetItem(i)
-    if (b) then inv:RemoveItem(item) end
-  end
+    local inv = rust.GetInventory(netuser)
+    if (not inv) then return end
+    for i=0,39 do
+        local b, item = inv:GetItem(i)
+        if (b) then inv:RemoveItem(item) end
+    end
 end 
 
 function PLUGIN:givePlayerKits()
-  -- give each netuser a kit 
-  -- PLUGIN.theKits[1] = { "Pistols" , { 99, "9mm Ammo"}, { 1, "9mm Pistol"}, { 5, "Large Medkit"} }
-local PL = Arena.playerList
-local KL = Arena.theKits[Arena.kitToUse]
-  print(KL)
-  for p=1,#Arena.playerList do
-    for k=1, (#Arena.theKits[Arena.kitToUse])-1 do
-        print((#Arena.theKits[Arena.kitToUse])-1 )
+    -- give each netuser a kit 
+    -- PLUGIN.theKits[1] = { "Pistols" , { 99, "9mm Ammo"}, { 1, "9mm Pistol"}, { 5, "Large Medkit"} }
+    local PL = Arena.playerList
+    local KL = Arena.theKits[Arena.kitToUse]
+    for p=1,#Arena.playerList do
+        for k=1, (#Arena.theKits[Arena.kitToUse])-1 do
+        --[[print((#Arena.theKits[Arena.kitToUse])-1 )
         print(PL[p].displayName)
         print(KL[k+1][2])
-      	--rust.RunServerCommand("inv.giveplayer ".. PL[p].displayName .. " " .. KL[k+1][2] .. " " .. KL[k+1][1])
+        rust.RunServerCommand("inv.giveplayer ".. PL[p].displayName .. " " .. KL[k+1][2] .. " " .. KL[k+1][1])]]
         rust.RunServerCommand('inv.giveplayer "'.. PL[p].displayName .. '" "' .. KL[k+1][2] .. '" "' .. KL[k+1][1] .. '"')
+        end
     end
-  end
 end
      
 
@@ -118,14 +115,14 @@ function PLUGIN:Coord( netuser )
 end
 
 function PLUGIN:displayArena( netuser )
-  rust.SendChatToUser( netuser, #Arena.playerList .. " participant(s):" )
-	for i, player in ipairs(Arena.playerList) do
-		rust.SendChatToUser( netuser, Arena.playerList[i].displayName)
-	end
+    rust.SendChatToUser( netuser, #Arena.playerList .. " participant(s):" )
+	  for i, player in ipairs(Arena.playerList) do
+		    rust.SendChatToUser( netuser, Arena.playerList[i].displayName)
+	  end
 end
 
 function PLUGIN:AHELP( netuser )
-	rust.SendChatToUser( netuser, "Use /join to tp to the Arena if it is open, /coord to check location" )
+	  rust.SendChatToUser( netuser, "Use /join to tp to the Arena if it is open, /coord to check location" )
     if (netuser:CanAdmin()) then
         rust.SendChatToUser( netuser, "Use /startarena and /stoparena to activate/deactivate arena" )
     end
@@ -133,69 +130,61 @@ end
 
 -- Called when the server is initialized
 function PLUGIN:OnServerInitialized()
-	print(Arena.Title .. " v" .. Arena.Version .. " loaded!")
-	print(Arena.theKits)
-	print(Arena.theKits[kitToUse])
+	  print(Arena.Title .. " v" .. Arena.Version .. " loaded!")
+	  print(Arena.theKits)
+	  print(Arena.theKits[kitToUse])
 end
 
 -- Tests if a value is contained in a table
 function PLUGIN:containsVal(t, val)
-	for _,v in ipairs(t) do
-		if (v == val) then
-			return true
-		end
-	end
-	return false
+	  for _,v in ipairs(t) do
+		    if (v == val) then return true end
+	  end
+	  return false
 end
 
 -- Removes a value from an array
 function PLUGIN:removeVal(t, val)
-	for i,v in ipairs(t) do
-		if (v == val) then
-			table.remove(t, i)
-			rust.BroadcastChat("Arena", val.displayName .. " died in the arena...")
-			return true
-		end
-	end
-
-	return false
+    for i,v in ipairs(t) do
+		    if (v == val) then
+			      table.remove(t, i)
+			      rust.BroadcastChat("Arena", val.displayName .. " died in the arena...")
+			      return true
+		    end
+	  end
+	  return false
 end
 
 -- called when someone is killed
 function PLUGIN:OnKilled( target, dmg )
-   if(Arena.isOn== true and Arena.playing== true)
-   then
-      if(dmg.attacker and dmg.attacker.client )
-         -- local player = dmg.attacker.client.netUser
-	 -- local playerattacker = player.displayName
-	 local component = target:GetComponent("HumanController")
-	 if (not component) then return end
-	 local victim = rust.NetUserFromNetPlayer(component.networkViewOwner)
-	 -- check to see if the victim is part of the arena match
-	 if( Arena:containsVal(Arena.playerList, victim)
-	 then
-	    -- the player was killed in the arena so remove him from the aliveplayers array
-	    Arena:removeVal(Arena.alivePlayers, victim)
-	    rust.BroadcastChat("Arena", playerattacker .. " has slain " .. victim.playerName)
-	    if(#Arena.alivePlayers == 1)
-	    then
-	      timer.Once(15, function()
-               message = "Arena Winner: " .. playerattacker
-               rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
-               Arena:arenaOFF()
-              end)
-	    end
-            elseif(#Arena.alivePlayers == 0)
-    	     then
-    	     timer.Once(15, function()
-               message = "Arena Finished (everyone died)"
-               rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
-               Arena:arenaOFF()
-              end)
-    	    end
-	 end
-      end
-   end
+    if(Arena.isOn== true and Arena.playing== true) then
+        if(dmg.attacker and dmg.attacker.client ) then
+            -- local player = dmg.attacker.client.netUser
+	          -- local playerattacker = player.displayName
+	          local component = target:GetComponent("HumanController")
+	          if(not component) then return end
+	          local victim = rust.NetUserFromNetPlayer(component.networkViewOwner)
+	          -- check to see if the victim is part of the arena match
+	          if( Arena:containsVal(Arena.playerList, victim) then
+	              -- the player was killed in the arena so remove him from the aliveplayers array
+	              Arena:removeVal(Arena.alivePlayers, victim)
+	              rust.BroadcastChat("Arena", playerattacker .. " has slain " .. victim.playerName)
+	              if(#Arena.alivePlayers == 1) then
+	                  timer.Once(15, function()
+                    message = "Arena Winner: " .. playerattacker
+                    rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
+                    Arena:arenaOFF()
+                    end)
+                elseif(#Arena.alivePlayers == 0) then
+    	              timer.Once(15, function()
+                    message = "Arena Finished (everyone died)"
+                    rust.RunServerCommand("notice.popupall \"" .. message .. "\"")
+                    Arena:arenaOFF()
+                    end)
+    	          end
+	          end
+        end
+    end
 end
 
 function PLUGIN:SendHelpText( netuser )
